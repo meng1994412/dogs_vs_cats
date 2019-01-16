c0873957d08932c8dc064f85bb60078e7a374f94
meng1994412 <sunnym412@gmail.com>
2019-01-16 14:37:38 -0600

    feat: build AlexNet from scratch by using Keras

---
+++ pipeline/nn/conv/alexnet.py
@@ -0,0 +1,74 @@
+# import packages
+from keras.models import Sequential
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+from keras.layers.core import Dense
+from keras.layers.core import Flatten
+from keras.layers.core import Dropout
+from keras.regularizers import l2
+from keras import backend as K
+
+class AlexNet:
+    @staticmethod
+    def build(width, height, depth, classes, reg = 0.0002):
+        # initialize the model along with the input shape to be
+        # "channels last" and channels dimension itself
+        model = Sequential()
+        inputShape = (height, width, depth)
+        chanDim = -1
+
+        # if we are using "channel first", update the input shape
+        # and channels dimension
+        if K.image_data_format() == "channel_first":
+            inputShape = (depth, height, width)
+            chanDim = 1
+
+        # Block #1: first CONV => RELU => POOL layer set
+        model.add(Conv2D(96, (11, 11), strides = (4, 4), input_shape = inputShape,
+            padding = "same", kernel_regularizer = l2(reg)))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis = chanDim))
+        model.add(MaxPooling2D(pool_size = (3, 3), strides = (2, 2)))
+        model.add(Dropout(0.25))
+
+        # Block #2 second CONV => RELU => POOL layer set
+        model.add(Conv2D(256, (5, 5), padding = "same", kernel_regularizer = l2(reg)))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis = chanDim))
+        model.add(MaxPooling2D(pool_size = (3, 3), strides = (2, 2)))
+        model.add(Dropout(0.25))
+
+        # Block #3: 3 * (CONV = RELU) => POOL
+        model.add(Conv2D(384, (3, 3), padding = "same", kernel_regularizer = l2(reg)))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis = chanDim))
+        model.add(Conv2D(384, (3, 3), padding = "same", kernel_regularizer = l2(reg)))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis = chanDim))
+        model.add(Conv2D(256, (3, 3), padding = "same", kernel_regularizer = l2(reg)))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis = chanDim))
+        model.add(MaxPooling2D(pool_size = (3, 3), strides = (2, 2)))
+        model.add(Dropout(0.25))
+
+        # Block #4: first FC => RELU layer set
+        model.add(Flatten())
+        model.add(Dense(4096, kernel_regularizer = l2(reg)))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+
+        # Block #5: second FC => RELU layer set
+        model.add(Dense(4096, kernel_regularizer = l2(reg)))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+
+        # softmax classifiers
+        model.add(Dense(classes, kernel_regularizer = l2(reg)))
+        model.add(Activation("softmax"))
+
+        # return the constructed network architecture
+        return model
